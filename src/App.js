c23b3270b00b224282ed633ae49ea11781c95829
qiaoxu <qiaoxu@mtedu.com>
2019-05-24 15:07:05 +0800

    react核心

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import JsxTest from './JsxTest'
+import CompType from './CompType'
+import StateTest from './StateTest'
+import CartSample from './CartSample'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* 自定义组件开头大写 */}
+            {/* <JsxTest /> */}
+            <JsxTest/>
+            {/*组件类型*/}
+            <CompType/>
+            {/*状态*/}
+            <StateTest/>
+            {/*购物车*/}
+            <CartSample/>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+//引入react：React.createElement() -> vdom是将JSX语法转成虚拟dom
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
