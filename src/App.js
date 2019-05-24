c4bfd3d372240eafa10e93bf348d00e30a39e90b
qiaoxu <qiaoxu@mtedu.com>
2019-05-24 20:04:52 +0800

    react组件化

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import JsxTest from './JsxTest'
 import CompType from './CompType'
 import StateTest from './StateTest'
 import CartSample from './CartSample'
+import style from './App.module.css'
+//全局引入
+// import Button from 'antd/lib/button'
+// import 'antd/dist/antd.css'
 
+//按需引入
+import {Button} from 'antd';
+
+import CommontList from './CommontList';
+import Hoc from './Hoc'
+
+import Composition from './Composition'
+
+import ContextTest from './ContextTest'
 function App() {
     return (
-        <div className="App">
+        <div className={style.img}>
             {/* 自定义组件开头大写 */}
             {/* <JsxTest /> */}
             <JsxTest/>
@@ -17,6 +30,22 @@ function App() {
             <StateTest/>
             {/*购物车*/}
             <CartSample/>
+
+            {/*antd按钮*/}
+            <Button type='primary'>Button</Button>
+
+            {/*容器组件&展示组件*/}
+            <CommontList />
+
+            {/*高阶组件*/}
+            <Hoc name='sofiya'/>
+
+            {/*组件复合*/}
+            <Composition />
+
+            {/*组件跨层级通信-Context*/}
+
+            <ContextTest />
         </div>
     );
 }
